484272c1841f7261eee0a75351c3026cdcf3e40b
Victor Rodrigues <victorgrodriguesm7@gmail.com>
2020-11-16 20:22:48 -0300

    Movie object created

---
+++ frontend/src/components/Movie/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import IMovieDTO from "../../interfaces/movieDto";
+import "./index.css";
+
+export default function Movie({id, title, year, director, synopsis}: IMovieDTO){
+    return (
+        <div className="movie">
+            <h1>{title}</h1>
+            <img src={`http://localhost:3333/images/${id}.jpg`}/>
+            <p className="year">({year})</p>
+        </div>
+    );
+}
