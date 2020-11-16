8afceeb95a38933ae9f1491ab5f0902a300ed1ca
Victor Rodrigues <victorgrodriguesm7@gmail.com>
2020-11-16 20:15:47 -0300

    Added SearchPage to the app

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-
+import "./globals.css";
+import SearchPage from "./pages/searchPage";
 
 export default function App() {
     return (
-        <div>
-
-        </div>
+        <SearchPage/>
     );
 }
