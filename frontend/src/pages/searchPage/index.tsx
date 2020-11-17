7d6f883e5d9edf46801bd0e9727c5482b3bc5628
Victor Rodrigues <victorgrodriguesm7@gmail.com>
2020-11-17 10:40:47 -0300

    Filter Search Added to the App

--- frontend/src/pages/searchPage/index.tsx
+++ frontend/src/pages/searchPage/index.tsx
@@ -6,26 +6,49 @@ import "./index.css";
 
 export default function SearchPage(){
     let [movies, setMovies] = useState(Array<MovieModel>());
+    let [filteredMovies, setFilteredMovies] = useState(Array<MovieModel>())
+    let [searchQuery, setSearchQuery] = useState("");
+
     let searchPageController = useMemo(() => new SearchPageController(), []);
+
     useEffect(() => {
         searchPageController.getMovies().then((listMovies) => {
             setMovies(listMovies);
         })
-    }, [movies, searchPageController])
+    }, [searchPageController])
+
+    function handleSearchMovie(event: React.FormEvent<HTMLFormElement>): void{
+        event.preventDefault()
+        
+        if (!searchQuery){
+            setFilteredMovies(movies);
+            return;
+        } 
+
+        let filtered = movies.filter(
+            (movie) => movie.title.toLowerCase().search(searchQuery.toLowerCase()) !== -1 
+        );
+        
+        setFilteredMovies(filtered);
+    }
+    
     return (
         <div>
             <section className="header">
                 <h1>Search Your Movie</h1>
             </section>
 
-            <form className="search-form">
-                <input type="text"/>
+            <form className="search-form" onSubmit={(event) => handleSearchMovie(event)}>
+                <input 
+                    type="text"
+                    onChange={(e) => setSearchQuery(e.target.value)}/>
                 <button>Search</button>
             </form>
 
             <h3>Sharing a few of our favourites movies</h3>
             <div className="movie-list">
                 {
+                    !filteredMovies.length ?
                     movies.map((movie) =>{
                         return (<Movie
                             key={movie.id}
@@ -35,7 +58,15 @@ export default function SearchPage(){
                             director={movie.director}
                             synopsis={movie.synopsis}
                         />);
-                    })
+                    }) : filteredMovies.map((movie) =>{
+                        return (<Movie
+                            key={movie.id}
+                            id={movie.id}
+                            title={movie.title}
+                            year={movie.year}
+                            director={movie.director}
+                            synopsis={movie.synopsis}
+                        />);})
                 }
             </div>
         </div>
