1463205f86f1bc3a959a187b6a1c9cb125239f9d
Victor Rodrigues <victorgrodriguesm7@gmail.com>
2020-11-17 10:40:12 -0300

    Added Alt attribute to the Movie img

--- frontend/src/components/Movie/index.tsx
+++ frontend/src/components/Movie/index.tsx
@@ -6,7 +6,7 @@ export default function Movie({id, title, year, director, synopsis}: IMovieDTO){
     return (
         <div className="movie">
             <h1>{title}</h1>
-            <img src={`http://localhost:3333/images/${id}.jpg`}/>
+            <img src={`http://localhost:3333/images/${id}.jpg`} alt={title}/>
             <p className="year">({year})</p>
         </div>
     );
