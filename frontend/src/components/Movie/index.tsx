5c1b97689772e5ab243e587de6b8dee3e633eb49
Victor Rodrigues <victorgrodriguesm7@gmail.com>
2020-11-18 15:30:32 -0300

    Now The image depends of the api baseUrl

--- frontend/src/components/Movie/index.tsx
+++ frontend/src/components/Movie/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import api from "../../services/api";
 import IMovieDTO from "../../interfaces/movieDto";
 import "./index.css";
 
@@ -6,7 +7,9 @@ export default function Movie({id, title, year, director, synopsis}: IMovieDTO){
     return (
         <div className="movie">
             <h1>{title}</h1>
-            <img src={`http://localhost:3333/images/${id}.jpg`} alt={title}/>
+            <img 
+                src={`${api.defaults.baseURL}images/${id}.jpg`} 
+                alt={title}/>
             <p className="year">({year})</p>
         </div>
     );
